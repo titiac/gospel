8ed1398668443f16db8c9c316a1a7f2747b3805d
lzp <1607344839@qq.com>
2022-11-01 15:49:05 +0800

    kkkcb

---
+++ gospel/src/test/java/com/example/gospel/GospelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gospel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GospelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
